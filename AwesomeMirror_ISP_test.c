b5e90050f805692245cc96c77bb5c81dadd76b99
psh2hero <psh2hero@naver.com>
2017-03-05 17:45:15 +0900

    Modify and test code insert

--- AwesomeMirror_ISP_test.c
+++ AwesomeMirror_ISP_test.c
@@ -1,6 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void GetSkinInfo(unsigned char *rgb, int wid, int hgt, int gridX, int gridY, int *A, int *B, int *C);
+void saveInfoFile(int *info, char *filename);
+
+#define GRIDX 10
+#define GRIDY 10
+void main(int argv, char **argc)
+{
+	int i;
+	int w, h;
+	unsigned char *rgb;
+	int *A, *B, *C;
+	FILE *in = NULL;
+	
+	in = fopen(argc[1], "rb");
+	if (in == NULL)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	w = atoi(argc[3]);
+	h = atoi(argc[4]);
+	
+	rgb = (unsigned char *)malloc(w*h*3);
+	A = (int *)malloc(GRIDX*GRIDY*sizeof(int));
+	B = (int *)malloc(GRIDX*GRIDY*sizeof(int));
+	C = (int *)malloc(GRIDX*GRIDY*sizeof(int));
+
+	for (i=0;i<w*h;i++)
+	{
+		fread(rgb+i*3, 1, 3, in);
+	}
+
+	GetSkinInfo(rgb, w, h, GRIDX, GRIDY, A, B, C);
+	
+	saveInfoFile(A, "A.txt");
+	saveInfoFile(B, "B.txt");
+	saveInfoFile(C, "C.txt");
+
+	free(rgb);
+	free(A);
+	free(B);
+	free(C);
+	
+	fclose(in);
+}
+
+void saveInfoFile(int *info, char *filename)
+{
+	FILE *out = NULL;
+	int i;
+	
+	out = fopen(filename, "wt");
+	for (i=0;i<GRIDX*GRIDY;i++)
+	{
+		fprintf(out, "%3d\n", info[i]);
+	}
+	
+	fclose(out);
+}
+
+void savePGMFile(unsigned char *gray, char *filename, int w, int h)
+{
+	FILE *out = NULL;
+	int i;
+	
+	out = fopen(filename, "wt");
+	fprintf(out, "P2\n%d %d\n255\n", w, h);
+	
+	for (i=0;i<w*h;i++)
+	{
+		fprintf(out, "%3d\n", gray[i]);
+	}
+	
+	fclose(out);
+}
+
+void savePPMFile(unsigned char *rgb, char *filename, int w, int h)
+{
+	FILE *out = NULL;
+	int i;
+	
+	out = fopen(filename, "wt");
+	fprintf(out, "P3\n%d %d\n255\n", w, h);
+	
+	for (i=0;i<w*h;i++)
+	{
+		fprintf(out, "%3d %3d %3d\n", rgb[i*3], rgb[i*3+1], rgb[i*3+2]);
+	}
+	
+	fclose(out);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
 void convertRgb2Gray(unsigned char *rgb, int wid, int hgt, unsigned char *gray);
 void convertRgb2Rb(unsigned char *rgb, int wid, int hgt, unsigned char *rb);
 void getBlemish(unsigned char *gray, int wid, int hgt, int gridX, int gridY, int *A);
@@ -18,9 +114,9 @@ void getDecolorize(unsigned char *rb, int wid, int hgt, int gridX, int gridY, in
 // - int gridY : Count of Vertical Grid
 // 
 // Output
-// - int *A : Array of Blemish(?창횈쩌)
-// - int *B : Array of Freckle(짹창쨔횑)
-// - int *C : Array of Decolorize(쨩철횁쨋횇쨩쨩철) 
+// - int *A : Array of Blemish(ÀâÆ¼)
+// - int *B : Array of Freckle(±â¹Ì)
+// - int *C : Array of Decolorize(»öÁ¶Å»»ö) 
 /************************************************************/
 void GetSkinInfo(unsigned char *rgb, int wid, int hgt, int gridX, int gridY, int *A, int *B, int *C)
 {
@@ -402,3 +498,4 @@ void getDecolorize(unsigned char *rb, int wid, int hgt, int gridX, int gridY, in
 		}
 	}
 }
+
